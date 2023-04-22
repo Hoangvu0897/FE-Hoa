ac6800acb09afd9285e005e71b067282ca545803
hoangvu0397 <thv0897@gmail.com>
2023-04-22 07:35:54 +0700

    4222023

--- App.js
+++ App.js
@@ -4,28 +4,46 @@ import Hoa from "./components/Hoa";
 import ListLoaiHoa from "./components/Listhoa";
 import Capnhatloaihoa from "./components/Capnhatloaihoa";
 import { Dangky } from "./components/Dangky";
-import { Dangnhap } from "./components/Dangnhap"
+import { Dangnhap } from "./components/Dangnhap";
+import React, { useState, createContext } from "react";
 const Stack = createNativeStackNavigator();
-
+export const AuthContext = createContext();
 export default function App() {
+  const [dangNhapThanhCong, setDangNhapThanhCong] = useState(false);
+  const [tendangnhap, gantendangnhap] = useState("");
+
+  const dangxuatnguoidung = () => {
+    setDangNhapThanhCong(false);
+    gantendangnhap("");
+  };
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HoaType">
-        <Stack.Screen name="Loại Hoa" component={ListLoaiHoa}></Stack.Screen>
-        <Stack.Screen name="Hoa" component={Hoa}></Stack.Screen>
-        <Stack.Screen
-          name="Capnhatloaihoa"
-          component={Capnhatloaihoa}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="dangkynguoidung"
-          component={Dangky}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="dangnhapnguoidung"
-          component={Dangnhap}
-        ></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider
+      value={{
+        dangNhapThanhCong,
+        setDangNhapThanhCong,
+        tendangnhap,
+        gantendangnhap,
+        dangxuatnguoidung,
+      }}
+    >
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="HoaType">
+          <Stack.Screen name="Loại Hoa" component={ListLoaiHoa}></Stack.Screen>
+          <Stack.Screen name="Hoa" component={Hoa}></Stack.Screen>
+          <Stack.Screen
+            name="Capnhatloaihoa"
+            component={Capnhatloaihoa}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="dangkynguoidung"
+            component={Dangky}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="dangnhapnguoidung"
+            component={Dangnhap}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
--- components/Dangnhap.js
+++ components/Dangnhap.js
@@ -1,43 +1,110 @@
-import React, { useState, useEffect } from "react";
-import { Button, Text, TextInput, View } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../App";
+
+import { Button, Text, TextInput, View, Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export const Dangnhap = () => {
-    const [tendangnhap, gantendangnhap] = useState("")
-    const [matkhau, ganmatkhau] = useState("")
-    const [nguoidung, setnguoidung] = useState("")
-    const dangnhapnguoidung = async () => {
-        try {
-            var nd = { tendangnhap, matkhau }
-            console.log(JSON.stringify(nd));
-            const response = await fetch('http://10.45.230.217:3000/nguoidungs/Login', {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(nd),
-            });
-            const json = await response.json()
-            console.log(json);
-        } catch (err) {
-            console.error(error);
-        } finally {
-        }
+  // const [tendangnhap, gantendangnhap] = useState("");
+  const [matkhau, ganmatkhau] = useState("");
+  const [nguoidung, setnguoidung] = useState("");
+  // const [dangNhapThanhCong, setDangNhapThanhCong] = useState(false);
+  const {
+    dangNhapThanhCong,
+    setDangNhapThanhCong,
+    tendangnhap,
+    gantendangnhap,
+    dangxuatnguoidung,
+  } = useContext(AuthContext);
 
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem("nguoidung", JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
     }
-    useEffect(() => {
-        dangnhapnguoidung()
-        console.log(nguoidung);
-    }, [])
+  };
 
-    return (
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("nguoidung");
+      if (value !== null) {
+        setnguoidung(JSON.parse(value));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const dangnhapnguoidung = async () => {
+    try {
+      var nd = { tendangnhap, matkhau };
+      console.log(JSON.stringify(nd));
+      const response = await fetch("http://192.168.1.8:3000/nguoidungs/Login", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(nd),
+      });
+      const json = await response.json();
+      console.log(json);
+      if (json) {
+        storeData(json);
+        setnguoidung(json);
+        setDangNhapThanhCong(true);
+        Alert.alert("Thông báo", "Đăng nhập thành công!");
+      } else {
+        setDangNhapThanhCong(false);
+        Alert.alert("Thông báo", "Đăng nhập thất bại!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // const dangxuatnguoidung = async () => {
+  //   try {
+  //     await AsyncStorage.removeItem("nguoidung");
+  //     setnguoidung(null);
+  //     setDangNhapThanhCong(false);
+  //     gantendangnhap("");
+  //     ganmatkhau("");
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <View>
+      {!dangNhapThanhCong ? (
+        <>
+          <Text>Tên Đăng Nhập</Text>
+          <TextInput
+            value={tendangnhap}
+            onChangeText={gantendangnhap}
+          ></TextInput>
+          <Text>Mật Khẩu</Text>
+          <TextInput
+            secureTextEntry={true}
+            value={matkhau}
+            onChangeText={ganmatkhau}
+          ></TextInput>
+          <Button title="Đăng Nhập" onPress={dangnhapnguoidung} />
+        </>
+      ) : (
         <View>
-            <Text>Tên Đăng Nhập</Text>
-            <TextInput value={tendangnhap} onChangeText={gantendangnhap}></TextInput>
-            <Text>Mật Khẩu</Text>
-            <TextInput secureTextEntry={true} value={matkhau} onChangeText={ganmatkhau}></TextInput>
-            <Button title="Đăng Nhập" onPress={dangnhapnguoidung} />
-            <TextInput value={(nguoidung ? nguoidung.tennguoidung : "")}></TextInput>
+          <Text>Xin chào, {tendangnhap}</Text>
+          <Button title="Đăng xuất" onPress={dangxuatnguoidung} />
         </View>
-    )
-}
-export default Dangnhap
+      )}
+    </View>
+  );
+};
+
+export default Dangnhap;
